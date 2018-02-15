09351326729114bb9c5920161c2f08a15621cdf8
Emre Filiz <e_filiz@fanshaweonline.ca>
2018-02-15 06:01:20 +0300

    Updated References.txt

--- admin/admin_index.php
+++ admin/admin_index.php
@@ -1,27 +1,40 @@
 <?php
 	require_once('phpscripts/config.php');
 	confirm_logged_in();
-	switch (true) {		
-    case date("H") >= 6:
-        $message = "Good Morning";
-    case date("H") >= 12:
-        $message = "Good Afternoon";        
-    case date("H") >= 16:
-        $message = "Good Evening";    
-    default:
-        $message = "Good Night";
-}
+	switch (true) {
+	case date("H") >= 6:
+		$message = "Good Morning";
+	case date("H") >= 12:
+		$message = "Good Afternoon";
+	case date("H") >= 16:
+		$message = "Good Evening";
+	default:
+		$message = "Good Night";
+	}
 ?>
 <!doctype html>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>Welcome to your admin panel</title>
-<link rel=stylesheet type=text/css href=css/style.css>
-</head>
-<body>
-	<?php echo $message,', ', $_SESSION['user_name'];  ?>
-	<br>
-	Last Login: <?php echo $_SESSION['user_last_login'];  ?>
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Welcome to your admin panel</title>
+		<!-- Font -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+		<!-- Compressed CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.min.css"/>
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<div class="card" style="width: 300px;">
+			<div class="card-divider">
+				<h4><?php echo $message,', ', $_SESSION['user_name'];  ?></h4>
+			</div>			
+			<div class="card-section">
+				Last Login: <?php echo $_SESSION['user_last_login'];  ?>				
+			</div>
+		</div>
+	</body>
+	<!-- Compressed JQuery & Foundation JavaScript -->
+	<script	src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.min.js"></script>
 </html>
